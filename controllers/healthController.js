4dcfe28ce009be79a3ffa834e86364d9816a919b
Aayush pal <Aayushpal3217@gmail.com>
2026-09-05 23:14:53 +0530

    Fix Bugs

--- controllers/healthController.js
+++
@@ -1,8 +0,0 @@
-
-exports.getHealth = (req, res) => {
-    res.status(200).json({
-        message: "Health is good"
-    });
-}
-
-module.exports = exports;
--- routes/healthRoute.js
+++
@@ -1,9 +0,0 @@
-const healthController = require('../controllers/healthController');
-
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/', healthController.getHealth);
-
-module.exports = router;
